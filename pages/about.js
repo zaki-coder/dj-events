fff2545f8ee455ab75cfbcee380b2d506b1c6cd4
zaki azizi <zakiazizi1841992@gmail.com>
2021-08-23 20:26:24 +0200

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find the latest DJ events and other musical events.</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
